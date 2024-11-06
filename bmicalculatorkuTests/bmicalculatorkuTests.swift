823fc2bf94819c0ae627d2bb36d84568ad77cfcc
Anang Kurniawan <anangk97@gmail.com>
2024-11-06 16:44:39 +0700

    Initial Commit

---
+++ bmicalculatorkuTests/bmicalculatorkuTests.swift
@@ -0,0 +1,17 @@
+//
+//  bmicalculatorkuTests.swift
+//  bmicalculatorkuTests
+//
+//  Created by Anang Kurniawan on 06/11/24.
+//
+
+import Testing
+@testable import bmicalculatorku
+
+struct bmicalculatorkuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
